57f355cc5330b32ece7c7b18d9f1b5f89fc40e03
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2021-08-29 16:29:36 +0700

    feat: add alternative tags

--- config.def.h
+++ config.def.h
@@ -30,6 +30,12 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -99,6 +105,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_0,                     tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,                 focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period,                focusmon,       {.i = +1 } },
+	{ MODKEY,                       XK_n,                     togglealttag,   {0} },
 	{ MODKEY|ShiftMask,             XK_comma,                 tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period,                tagmon,         {.i = +1 } },
   { 0,                            XF86XK_MonBrightnessUp,   spawn,          {.v = xicmd } },
