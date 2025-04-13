716aeb28c33dabe5d9804e8b1b3e3942e9a060ae
Fikri Rahmat Nurhidayat <fikrirnurhidayat@gmail.com>
2025-04-13 17:57:46 +0700

    apply initial systray patch

--- config.def.h
+++ config.def.h
@@ -7,19 +7,24 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 32;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int horizpadbar        = 16;       /* horizontal padding for statusbar */
-static const int vertpadbar         = 32;       /* vertical padding for statusbar */
-static const char *fonts[]          = { "monospace:size=11:antialias=true:hinting=true:style=Bold", "Symbols Nerd Font Mono:size=11:style=Regular:antialias=true:hinting=true" };
-static const char dmenuvertpad[]    = "16";
-static const char dmenulines[]      = "4";
-static char normbgcolor[]           = "#222222";
-static char normbordercolor[]       = "#444444";
-static char normfgcolor[]           = "#bbbbbb";
-static char selfgcolor[]            = "#eeeeee";
-static char selbordercolor[]        = "#005577";
-static char selbgcolor[]            = "#005577";
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray         = 1;        /* 0 means no systray */
+static const int showbar             = 1;        /* 0 means no bar */
+static const int topbar              = 1;        /* 0 means bottom bar */
+static const int horizpadbar         = 16;       /* horizontal padding for statusbar */
+static const int vertpadbar          = 32;       /* vertical padding for statusbar */
+static const char *fonts[]           = { "monospace:size=11:antialias=true:hinting=true:style=Bold", "Symbols Nerd Font Mono:size=11:style=Regular:antialias=true:hinting=true" };
+static const char dmenuvertpad[]     = "16";
+static const char dmenulines[]       = "4";
+static char normbgcolor[]            = "#222222";
+static char normbordercolor[]        = "#444444";
+static char normfgcolor[]            = "#bbbbbb";
+static char selfgcolor[]             = "#eeeeee";
+static char selbordercolor[]         = "#005577";
+static char selbgcolor[]             = "#005577";
 static char dormfgcolor[]            = "#444444";
 static char dormbordercolor[]        = "#222222";
 static char dormbgcolor[]            = "#222222";
